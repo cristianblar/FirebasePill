84c27d71ec9d28289efbbeeb4027fa0d4a9f6cc9
cristianblar <cristian@cristianblar.dev>
2022-02-24 06:18:27 -0500

    feat: create basic form for sign up

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-// Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
+
+const form = document.createElement('form');
+form.innerHTML = `
+  <label for="email">Email</label>
+  <input type="text" id="email" required>
+  <label for="password" required>Password</label>
+  <input type="password" id="password">
+  <input type="submit" id="registrationButton" value="Registrarse">
+`;
+
+document.querySelector('#root').appendChild(form);
 
 myFunction();

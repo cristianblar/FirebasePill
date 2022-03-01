cdbad22ad6d38a6e07b19a4de41ea22bbf86e659
cristianblar <cristian@cristianblar.dev>
2022-03-01 14:40:20 -0500

    chore: add testing for auth logic

--- src/lib/firebase-auth.js
+++ src/lib/firebase-auth.js
@@ -1,23 +1,15 @@
 import { createUserWithEmailAndPassword, getAuth } from './firebase-init.js';
 
 export function createUser(emailInputValue, passwordInputValue) {
+  if (!emailInputValue || !passwordInputValue) {
+    throw new TypeError('Missing email and/or password');
+  }
+
   const auth = getAuth();
 
   return createUserWithEmailAndPassword(
     auth,
     emailInputValue,
     passwordInputValue,
-  )
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      console.log(errorCode, errorMessage);
-    });
+  );
 }
